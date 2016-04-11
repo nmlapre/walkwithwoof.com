467ed0f933a21a511c5dbe35cd86686dad850c86
Nathan LaPré <nmlapre@users.noreply.github.com>
2016-04-10 21:34:25 -0700

    removing ajax for tests

--- assets/js/main.js
+++ assets/js/main.js
@@ -141,7 +141,7 @@
 
 			// Events.
 			// Note: If you're *not* using AJAX, get rid of this event listener.
-			
+			/*
 				$form.addEventListener('submit', function(event) {
 
 					event.stopPropagation();
@@ -187,7 +187,7 @@
 						}, 750);
 
 				});
-				
+			*/	
 
 		})();
 
